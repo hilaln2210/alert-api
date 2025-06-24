150a038ad50f83b5d251c4cad336eab7335d6cf9
hilaln2210 <156947582+hilaln2210@users.noreply.github.com>
2025-06-25 00:57:20 +0300

    server.py

--- server.py
+++ server.py
@@ -1,22 +1,28 @@
 from flask import Flask, jsonify
 import requests
-import datetime
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_alerts():
-    today = datetime.datetime.now().strftime('%d.%m.%Y')
-    url = f'https://alerts-history.oref.org.il/Shared/Ajax/GetAlarmsHistory.aspx?lang=he&fromDate={today}&toDate={today}&mode=0'
+    today = datetime.now().strftime("%d.%m.%Y")
+    url = f"https://alerts-history.oref.org.il/Shared/Ajax/GetAlarmsHistory.aspx?lang=he&fromDate={today}&toDate={today}&mode=0"
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
+    }
 
     try:
-        response = requests.get(url)
+        response = requests.get(url, headers=headers)
         response.raise_for_status()
         data = response.json()
-        filtered_alerts = [item for item in data if item.get("category") == 1]
-        return jsonify({'alerts': filtered_alerts})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+
+        filtered_alerts = [alert for alert in data if alert.get('category') == 1]
+        return jsonify({"alerts": filtered_alerts})
+
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    app.run(host="0.0.0.0", port=10000)
